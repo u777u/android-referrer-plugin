cfc17d6fad2789662c268d8ff803eb83d8b518c2
nyancodeid <ryandevstudio@gmail.com>
2019-01-27 01:24:17 +0700

    adding package.json

--- src/com/eightz/mobile/cordova/plugin/android/referrer/Receiver.java
+++ src/com/eightz/mobile/cordova/plugin/android/referrer/Receiver.java
@@ -12,21 +12,19 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class Receiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
 
-@Override
-public void onReceive(Context context, Intent intent) {
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            String referrerString = extras.getString("referrer");
+            if (referrerString != null) {
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
 
-    Bundle extras = intent.getExtras();
-    if (extras != null) {
-    	String referrerString = extras.getString("referrer");
-        if (referrerString != null) {
-    	    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-
-    	    Editor edit = sharedPreferences.edit();
-    	    edit.putString("referrer", referrerString);
-    	    edit.commit();
+                Editor edit = sharedPreferences.edit();
+                edit.putString("referrer", referrerString);
+                edit.commit();
+            }
         }
     }
-}
- 
 } // end of class
